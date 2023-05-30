44c00ec964d4a2bcb233a833b5e0b185af0ce88f
akins-dev <bobemmanuel503@gmail.com>
2023-05-30 02:26:16 +0100

    fix

--- components/Header.js
+++ components/Header.js
@@ -28,7 +28,7 @@ function Header() {
 
       {/* Right */}
       <div className='flex items-center justify-end text-black'>
-        <p className='hidden md:inline-flex  hover:bg-gray-100 rounded-full px-4 py-2 text-sm cursor-pointer'>Airbnb your home</p>
+        <p className='hidden lg:inline-flex hover:bg-gray-100 font-medium rounded-full px-4 py-2 text-sm cursor-pointer'>Airbnb your home</p>
         
         <div className="hidden md:inline-flex hover:bg-gray-100 rounded-full px-4 py-2">
           <GlobeAltIcon className='h-5 cursor-pointer' />
