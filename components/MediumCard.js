0c75a02c1012ef3cb6d166a684d665c0c4b622f1
akins-dev <bobemmanuel503@gmail.com>
2023-05-30 04:17:50 +0100

    medium

--- components/MediumCard.js
+++ components/MediumCard.js
@@ -2,7 +2,12 @@ import Image from 'next/image'
 
 function MediumCard({ img, title }) {
   return (
-    <div>MediumCard</div>
+    <div className="cursor-pointer hover:scale-105 transition transform duration-300 ease-out">
+        <div className="relative h-80 w-80">
+            <Image src={img} fill={true} alt="Image"/>
+        </div>
+        <h3 className="text-2xl mt-3">{title}</h3>
+    </div>
   )
 }
 
