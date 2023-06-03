67e7b31d5e40d223e2d6d745395098a89af2d72c
akins-dev <bobemmanuel503@gmail.com>
2023-06-03 08:22:00 +0100

    chore: done with this project

--- components/InfoCard.jsx
+++ components/InfoCard.jsx
@@ -4,7 +4,7 @@ import { StarIcon } from "@heroicons/react/solid";
 
 function InfoCard({ img, location, title, description, star, price, total }) {
   return (
-    <div className="flex py-7 px-2 border-b cursor-pointer hover:opacity-80 hover:shadow-lg pr-4 transition duration-200 ease-out first:border-t">
+    <div className="flex py-7 px-2 border-b cursor-pointer hover:opacity-80 hover:shadow-lg pr-4 transition duration-200 ease-out first:border-t disable-tap-highlight">
         <div className="relative h-24 w-40 md:h-52 md:w-80 flex-shrink-0">
             <Image 
                 alt="image" 
@@ -15,26 +15,26 @@ function InfoCard({ img, location, title, description, star, price, total }) {
             />
         </div>
         <div className="flex flex-col flex-grow pl-5">
-            <div className="flex justify-between">
+            <div className="hidden md:inline-flex justify-between">
                 <p>{ location }</p>
                 <HeartIcon className="h-7 cursor-pointer" />
             </div>
 
-             <h4 className="text-xl">{title}</h4>
+             <h4 className="hidden md:inline-flex text-xl">{title}</h4>
              {/* custom line */}
-             <div className="border-b w-10 pt-2"/>
+             <div className="hidden md:inline-flex border-b w-10 pt-2"/>
 
-             <p className="pt-2 text-sm text-gray-500 flex-grow">{description}</p>
+             <p className="hidden md:inline-flex pt-2 text-sm text-gray-500 flex-grow">{description}</p>
 
-             <div className="flex justify-between items-end pt-5">
-                <p className="flex items-center">
+             <div className="flex justify-between items-end lg:pt-5">
+                <p className="hidden md:inline-flex items-center">
                     <StarIcon className="h-5 text-red-400" />
                     {star}
                 </p>
 
                 <div className="">
-                    <p className="text-lg font-semibold lg:text-2xl pb-2">{price}</p>
-                    <p className="text-right font-extralight">{total}</p>
+                    <p className="md:text-lg font-semibold lg:text-2xl pb-2">{price}</p>
+                    <p className="md:text-right font-extralight">{total}</p>
                 </div>
              </div>
         </div>
